1293d123c9cbecc0193a7f747b8b5c6114fe9289
Gabriel Sanchez <124187943+InTheLittleMoon@users.noreply.github.com>
2023-04-30 02:05:15 -0700

    wip entry page

--- src/pages/entry-page/entry-sectionLeft.js
+++ src/pages/entry-page/entry-sectionLeft.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./entry-sectionLeft.css";
+
+//imported components
+import ImageCarousel from "../../components/entry-page-image-carousel/image-carousel";
 
 //imported images
 import fpLogo from "../../assets/images/entry-page_left-section-top-dummyLogo.jpg";
 
 export default function SectionLeft() {
-  //held states
-  const [imageArray, setImageArray] = useState([]);
 
-  //useEffect(() => {}, []);
   return (
     <div className="entry-page_left-section-container">
       <div className="left-section-top">
@@ -18,6 +18,7 @@ export default function SectionLeft() {
       </div>
       <div className="left-section-mid">mid</div>
       {/* make sliding array items linked to section bot array items as they move */}
+      <ImageCarousel />
       <div className="left-section-bot">
         {/* make sliding array items linked to section mid images as they move */}
       </div>
