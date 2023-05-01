af657bb598c6914421cbe1e7595bf7a6f85bbc2d
Gabriel Sanchez <g.sanchez1216@gmail.com>
2023-05-01 09:29:23 -0700

    WIP user profile page

--- src/pages/user-profile-page/user-profile-page.js
+++ src/pages/user-profile-page/user-profile-page.js
@@ -1,10 +1,36 @@
 import React from "react";
 import "./user-profile-page.css";
 
+//image imports
+import searchIcon from "../../assets/images/user-profile_search-bar-icon.png";
+
 export default function UserProfilePage() {
+  //handles upper search-bar inputs
+  const searchHandler = () => {
+    console.log("searched");
+  };
+
   return (
     <React.Fragment>
-      <div className="user-profile_upper-nav-link-container">HI</div>
+      <div className="user-profile_upper-nav-link-container">
+        <div className="search-bar-container">
+          <img alt="fp-logo"></img>
+          <div className="search-bar">
+            <input></input>
+            <img
+              alt="search-icon"
+              src={searchIcon}
+              onClick={searchHandler}
+            ></img>
+          </div>
+        </div>
+        <div className="linked-icons-container">
+          <span className="icon">Home</span>
+          <span className="icon">Friends</span>
+          <span className="icon">Messages</span>
+          <span className="icon">Notifications</span>
+        </div>
+      </div>
       <div className="user-profile_page-container">
         <div className="user-profile_nav-link-container">HI</div>
         <div className="user-profile_main-container">
